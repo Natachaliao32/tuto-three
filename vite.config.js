d0b11e4afcce02a1b847c03c4c079208fccda57b
Natacha <natachaliao32@gmail.com>
2022-02-13 01:35:53 +0100

    adding build.rollupOptions.external
    build success but preview fail

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 export default {
     build: {
-        chunkSizeWarningLimit: 700
+        chunkSizeWarningLimit: 700,
+        rollupOptions: {
+            external: [
+                "three",
+                "three/examples/jsm/loaders/OBJLoader.js",
+                "three/examples/jsm/controls/OrbitControls.js",
+                "three/examples/jsm/loaders/TGALoader.js",
+                "three/examples/jsm/loaders/FBXLoader.js"
+            ]
+        }
     },
     base: '/tuto-three/'
 }
